375fd683ec96340ea20efa5674de821f259d3af8
eyegan <guillaury.gv40@gmail.com>
2024-07-21 16:44:54 +0200

    c/c du css de notesview/notescard vers postcard/postview
    je n'étais pas inspiré pour les post alors c/c css, je verrais le reste plus tard

--- frontendvue2/src/components/PostCard.vue
+++ frontendvue2/src/components/PostCard.vue
@@ -1,14 +1,9 @@
 <template>
-  <div class="home">
-    <div class="post">
-      <p class="content">{{ post.id }}</p>
-      <h2 class="titre">{{ post.title }}</h2>
-      <p class="content">{{ post.created_at }}</p>
-      <p class="content">{{ post.content }}</p>
-      <p class="content">{{ post.language }}</p>
-      <p class="content">{{ post.category }}</p>
-    </div>
-
+  <div class="post">
+    <h2 class="title">{{ post.title }}</h2>
+    <p class="content">{{ post.content }}</p>
+    <p class="content">{{ post.language }}</p>
+    <p class="content">{{ post.category }}</p>
   </div>
 </template>
 
@@ -21,22 +16,34 @@ export default {
 };
 </script>
 
-</script>
-
 <style scoped>
-.home {
-  display: flex;
-}
 .post {
-  width: 200px;
-}
-.titre {
-  border: 1px solid black;
-}
-.content {
-  border: 2px solid black;
-}
-.heart {
   display: flex;
+  flex-direction: column;
+  align-items: center; /* Centre les éléments à l'intérieur de la carte */
+  justify-content: space-between; /* Distribue l'espace */
+  margin: 10px; /* Marge uniforme pour l'espacement entre les cartes */
+  padding: 20px; /* Rembourrage à l'intérieur de la carte */
+  background-color: #f9f9f9; /* Couleur de fond plus douce */
+  border: 1px solid #ccc; /* Bordure subtile */
+  box-shadow: 0 4px 8px rgba(0,0,0,0.1); /* Ombre douce pour un effet 3D */
+  border-radius: 45px; /* Coins arrondis */
+  width: 300px; /* Largeur fixe */
+  height: auto; /* Hauteur automatique */
+  overflow: hidden; /* Assure qu'il n'y ait pas de débordement */
+  text-overflow: ellipsis; /* Empêche le texte de déborder */
+  white-space: normal; /* Permet le retour à la ligne du texte */
+}
+
+.title {
+  margin-top: 0; /* Supprime la marge en haut du titre */
+  color: #333; /* Couleur foncée pour le texte pour une meilleure lisibilité */
+  font-size: 18px; /* Taille de police légèrement plus grande */
+}
+
+.content {
+  color: #666; /* Couleur de texte plus claire pour le contenu */
+  font-size: 16px; /* Taille de police lisible */
+  line-height: 1.5; /* Espacement des lignes amélioré */
 }
 </style>
--- frontendvue2/src/views/PostView.vue
+++ frontendvue2/src/views/PostView.vue
@@ -1,40 +1,54 @@
 <template>
   <div class="home">
-    <div class="tri">
-      <ul>
-        <li>pagination</li>
-        <li>search bar</li>
-        <li>crud</li>
-        <li></li>
-      </ul>
-    </div>
-    <div>
-
-      <input class="input" type="text" v-model="searchQuery" placeholder="Enter search term" />
+    <div class="search">
+      <input
+        class="input"
+        type="text"
+        v-model="searchQuery"
+        placeholder="saississez votre recherche..."
+      />
       <button class="button-search" @click="searchPost">Search</button>
+    </div>
+    
+    <div class="posts">
       <PostCard v-for="post in posts" :key="post.id" :post="post" />
-      <div class="pagination">
-        <button class="button" @click="prevPage" :disabled="currentPage === 1">page précédente </button>
-        <span> Page {{ currentPage }} of {{ numPages }}</span>
-        <button class="button" @click="nextPage" :disabled="currentPage === numPages">    page suivante    </button>
-      </div>
+    </div>
+    
+    <div class="pagination">
+      <button
+        class="button"
+        @click="prevPage"
+        :disabled="currentPage === 1"
+        v-if="currentPage > 1"
+      >
+        page précédente
+      </button>
+      <span :data-current-page="currentPage"> Page {{ currentPage }} of {{ numPages }}</span>
+      <button
+        class="button"
+        @click="nextPage"
+        :disabled="currentPage === numPages"
+        v-if="currentPage < numPages"
+      >
+        page suivante
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-import PostCard from '../components/PostCard.vue';
+import axios from "axios";
+import PostCard from "../components/PostCard.vue";
 
 export default {
-  name: 'PostView',
+  name: "PostView",
   components: { PostCard },
   data() {
     return {
       posts: [],
       currentPage: 1,
       numPages: 1,
-      searchQuery: ''
+      searchQuery: "",
     };
   },
   mounted() {
@@ -45,8 +59,9 @@ export default {
       const url = this.searchQuery
         ? `/api/v1/post/?search=${this.searchQuery}&page=${this.currentPage}`
         : `/api/v1/post/?page=${this.currentPage}`;
-        
-      axios.get(url)
+
+      axios
+        .get(url)
         .then((response) => {
           this.posts = response.data.posts;
           this.numPages = response.data.num_pages;
@@ -57,7 +72,7 @@ export default {
         });
     },
     searchPost() {
-      this.currentPage = 1;  // Reset to first page on new search
+      this.currentPage = 1; // Reset to first page on new search
       this.getPosts();
     },
     nextPage() {
@@ -84,28 +99,75 @@ export default {
 .button-search {
   border: 1px solid black;
   margin: 5px;
-
 }
 .home {
   background-color: aqua;
-  height: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
 }
-.tri {
-  background-color: yellow;
-  width: 200px;
-  height: 700px;
+.posts {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap; /* Permet aux cartes de passer à la ligne suivante si l'espace horizontal est insuffisant */
+  width: 70vw;
+  height: 55vh;
+  gap: 5%;
+  border: 10px solid black;
+  border-radius: 75px;
 }
 .pagination {
   display: flex;
+  align-items: center;
   justify-content: center;
   margin: 20px;
 }
-.button {
-  border: 2px solid red;
-  margin: 0px 10px;
+.pagination > span {
+  background-color: white;
+  border-radius: 25px;
+  padding: 5px;
+  font-size: 15px;
+}
 
+.button {
+  margin: 0px 10px;
+  background-color: white;
+  border-radius: 25px;
+  padding: 5px;
+  font-size: 15px;
+}
+.search {
+  border: 1px solid gray;
+  margin: 1%;
+  padding: 15px;
+  border-radius: 25px;
+}
+.input {
+  background-color: white;
+  padding: 5px;
+  border: none;
+  border-radius: 25px;
+}
+.input::placeholder {
+  color: black;
+  font-size: 15px;
+  letter-spacing: -0.5px;
+  word-spacing: -0.1ch;
+}
+.input:focus {
+  outline: none; /* Supprime l'outline par défaut pour avoir un contrôle complet sur le style */
+  border: none; /* Ajoute une bordure plus épaisse et change sa couleur */
+}
+.button-search {
+  background-color: white;
+  color: black;
+  padding: 5px;
+  border: none;
+  border-radius: 25px;
+  transition: background-color 0.3s ease, transform 0.3s ease; /* Ajout d'animations pour la couleur de fond et la transformation */
+}
+.button-search:hover {
+  transform: scale(1.05); /* Agrandit légèrement le bouton */
 }
 </style>
